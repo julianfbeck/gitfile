9061d385b6989ed0cadc2c1becf9567b348a80fe
Julian Beck <ju-fa-beck@t-online.de>
2019-07-27 21:27:33 +0200

    moved delete files

--- test.js
+++ test.js
@@ -3,6 +3,11 @@ const chai = require("chai");
 const gitFile = require("./gitFile")
 const fs = require('fs')
 
+fs.unlinkSync("test1")
+fs.unlinkSync("test2")
+fs.unlinkSync("./index.js")
+fs.unlinkSync("./discoverLocalWebservices.sh")
+
 
 
 describe('Testing', async () => {
@@ -12,7 +17,6 @@ describe('Testing', async () => {
         }
         await gitFile(cli)
         expect(fs.existsSync("./index.js")).to.equal(true)
-        fs.unlinkSync("./index.js")
     });
     it('Downloading from Gist', async () => {
         let cli = {
@@ -20,7 +24,6 @@ describe('Testing', async () => {
         }
         await gitFile(cli)
         expect(fs.existsSync("./discoverLocalWebservices.sh")).to.equal(true)
-        fs.unlinkSync("./discoverLocalWebservices.sh")
     });
     it('Downloading from Gitub with Filename', async () => {
         let cli = {
@@ -28,7 +31,6 @@ describe('Testing', async () => {
         }
         await gitFile(cli)
         expect(fs.existsSync("test1")).to.equal(true)
-        fs.unlinkSync("test1")
     });
     it('Downloading from Gist with Filename', async () => {
         let cli = {
@@ -36,7 +38,6 @@ describe('Testing', async () => {
         }
         await gitFile(cli)
         expect(fs.existsSync("test2")).to.equal(true)
-        fs.unlinkSync("test2")
 
     });
 });
